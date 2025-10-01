4450878b1bc506138acdf3edea4ff3ddc97957b3
Sara <sara707ab@gmail.com>
2025-10-01 16:38:54 -0300

    feat redirection to alumnos

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlumnoController;
 
 Route::resource('alumnos', AlumnoController::class);
+
+Route::get('/', function () {
+    return redirect()->route('alumnos.index');
+});
